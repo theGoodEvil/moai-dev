fdce0b3a14698d34a92b6d8c4c7921699cb4d810
Guido Lorenz <code@superquadratic.net>
2013-10-31 16:40:34 +0100

    Update Crittercism SDK to 4.1.2

--- src/moai-iphone/MOAICrittercismIOS.mm
+++ src/moai-iphone/MOAICrittercismIOS.mm
@@ -20,8 +20,6 @@
 	@text	Initialize Crittercism.
 	
 	@in		string appId			Available in Crittercism dashboard settings.
-	@in		string appKey			Available in Crittercism dashboard settings.
-	@in		string appSecret		Available in Crittercism dashboard settings.
 	@out	nil
 */
 int MOAICrittercismIOS::_init ( lua_State* L ) {
@@ -29,21 +27,12 @@ int MOAICrittercismIOS::_init ( lua_State* L ) {
 	MOAILuaState state ( L );
 
 	cc8* appId = lua_tostring ( state, 1 );
-	cc8* appKey = lua_tostring ( state, 2 );
-	cc8* appSecret = lua_tostring ( state, 3 );
 
 	NSString* ID = [[ NSString alloc ] initWithUTF8String:appId ];
-	NSString* key = [[ NSString alloc ] initWithUTF8String:appKey ];
-	NSString* secret = [[ NSString alloc ] initWithUTF8String:appSecret ];
 		
-	UIWindow* window = [[ UIApplication sharedApplication ] keyWindow ];
-	UIViewController* rootVC = [ window rootViewController ];
-		
-	[ Crittercism initWithAppID: ID andKey: key andSecret: secret andMainViewController: rootVC ];
+	[ Crittercism enableWithAppID: ID ];
 
 	[ ID release ];
-	[ key release ];
-	[ secret release ];
 		
 	return 0;
 }
