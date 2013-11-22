5c0beec78c61fc7b5006c790296132cb33dbe381
Guido Lorenz <code@superquadratic.net>
2013-11-22 16:27:25 +0100

    Support PKM files with ETC1 texture compression on Android

--- src/moai-sim/MOAIPkmHeader.h
+++ src/moai-sim/MOAIPkmHeader.h
@@ -11,19 +11,25 @@
 //================================================================//
 class MOAIPkmHeader :
 	public MOAITextureHeader {
+public:
+
+	enum {
+		ETC1_RGB_NO_MIPMAPS = 0x0000,
+	};
+
 private:
-	
+
 	static const u32 HEADER_SIZE		= 16;
 	static const u32 PKM_FILE_MAGIC		= 0x204d4b50;
 	static const u16 PKM_VERSION_1		= 0x3031;
 	
 	u32 mPKM;				// should be 'P' 'K' 'M' ' '
 	u16 mVersion;			// should be '1' '0'
-	u16 mType;				// should be 0 (ETC1_RGB_NO_MIPMAPS)
-	u16 mDataWidth;			// pixel width of image data (multiple of 4)
-	u16 mDataHeight;		// pixel height of image data (multiple of 4)
-	u16 mWidth;				// image pixel width (can be less due to padding)
-	u16 mHeight;			// image pixel height (can be less due to padding)
+	u8 mType [ 2 ];			// should be ETC1_RGB_NO_MIPMAPS, 2-byte big endian
+	u8 mDataWidth [ 2 ];	// pixel width of image data (multiple of 4), 2-byte big endian
+	u8 mDataHeight [ 2 ];	// pixel height of image data (multiple of 4), 2-byte big endian
+	u8 mWidth [ 2 ];		// image pixel width (can be less due to padding), 2-byte big endian
+	u8 mHeight [ 2 ];		// image pixel height (can be less due to padding), 2-byte big endian
 
 public:
 
@@ -32,6 +38,7 @@ public:
 	virtual void*			GetHeaderStart			();
 	virtual u32				GetHeight				();
 	virtual u32				GetTotalSize			();
+	u16						GetType					();
 	virtual u32				GetWidth				();
 	virtual bool			IsValid					();
 	virtual void			Invalidate				();
