22b340b8221d3cd891690010dd3afc09a39492ea
51MONSTER2 <simon@thegoodevil.com>
2017-03-09 13:47:47 +0100

    Fixes to tracking for IOS

--- src/host-ios/Classes/DWTracking.mm
+++ src/host-ios/Classes/DWTracking.mm
@@ -16,6 +16,16 @@ void dwTrackingLogProgress( cc8* msg ) {
     if (! isTrackerInitalized ) return;
     ATTracker* tracker = [[ATInternet sharedInstance] defaultTracker];
     
+    NSString* path = [NSString stringWithUTF8String:msg];
+    NSLog( @"msg is %@ path is %@", @(msg), path);
+    NSRange pageSeperatorRange = [path rangeOfString: @"::" options: NSBackwardsSearch ];
+    
+    NSString* page = [path substringToIndex: pageSeperatorRange.location ];
+    
+    NSString* chapter = [path substringFromIndex: (pageSeperatorRange.location + pageSeperatorRange.length)];
+    
+    NSLog( @"Page is %@ chapter is %@", page, chapter );
+    
     ATScreen *screen = [tracker.screens addWithName: @( msg ) ];
     screen.level2 = 34;
 
@@ -23,19 +33,30 @@ void dwTrackingLogProgress( cc8* msg ) {
     [tracker.customVars addWithId:2 value:@"34" type:ATCustomVarTypeScreen];
     [tracker.customVars addWithId:3 value:@"" type:ATCustomVarTypeScreen];
     [tracker.customVars addWithId:4 value:@"" type:ATCustomVarTypeScreen];
-    [tracker.customVars addWithId:5 value:@"" type:ATCustomVarTypeScreen]; // Page Name
+    [tracker.customVars addWithId:5 value:page type:ATCustomVarTypeScreen]; // Page Name
     [tracker.customVars addWithId:6 value:@"1" type:ATCustomVarTypeScreen];
     [tracker.customVars addWithId:7 value:@"" type:ATCustomVarTypeScreen];
     [tracker.customVars addWithId:8 value:@"" type:ATCustomVarTypeScreen];
     [tracker.customVars addWithId:9 value:@"20150101" type:ATCustomVarTypeScreen];
-    [tracker.customVars addWithId:10 value:@"" type:ATCustomVarTypeScreen]; // Chapter name
+    [tracker.customVars addWithId:10 value:chapter type:ATCustomVarTypeScreen]; // Chapter name
 
+    [tracker.customVars addWithId:1 value:@"68" type:ATCustomVarTypeApp];
+    [tracker.customVars addWithId:2 value:@"34" type:ATCustomVarTypeApp];
+    [tracker.customVars addWithId:3 value:@"" type:ATCustomVarTypeApp];
+    [tracker.customVars addWithId:4 value:@"" type:ATCustomVarTypeApp];
+    [tracker.customVars addWithId:5 value:page type:ATCustomVarTypeApp]; // Page Name
+    [tracker.customVars addWithId:6 value:@"1" type:ATCustomVarTypeApp];
+    [tracker.customVars addWithId:7 value:@"" type:ATCustomVarTypeApp];
+    [tracker.customVars addWithId:8 value:@"" type:ATCustomVarTypeApp];
+    [tracker.customVars addWithId:9 value:@"20150101" type:ATCustomVarTypeApp];
+    [tracker.customVars addWithId:10 value:chapter type:ATCustomVarTypeApp]; // Chapter name
+    
     if (tracker == nil) {
 	    NSLog(@"%@", @"default Instance is null before dispatch" );
 	    return;
     }
 
-    [tracker dispatch];
+    [screen sendView];
 
 
 }
