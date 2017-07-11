7c3ab92c1977d10e86f1448a9f506e492dd99dc9
Mischa Krempel <mischa@thegoodevil.com>
2017-07-11 14:56:41 +0200

    Changed order of tracking calls as parameters need to be set before the view is created.

--- src/host-ios/Classes/DWTracking.mm
+++ src/host-ios/Classes/DWTracking.mm
@@ -25,32 +25,22 @@ void dwTrackingLogProgress( cc8* msg ) {
     NSString* chapter = [path substringFromIndex: (pageSeperatorRange.location + pageSeperatorRange.length)];
     
     NSLog( @"Page is %@ chapter is %@", page, chapter );
+
     
-    ATScreen *screen = [tracker.screens addWithName: @( msg ) ];
+     [tracker.customVars addWithId:1 value:@"68" type:ATCustomVarTypeApp];
+     [tracker.customVars addWithId:2 value:@"34" type:ATCustomVarTypeApp];
+     [tracker.customVars addWithId:3 value:@"" type:ATCustomVarTypeApp];
+     [tracker.customVars addWithId:4 value:@"" type:ATCustomVarTypeApp];
+     [tracker.customVars addWithId:5 value:chapter type:ATCustomVarTypeApp];
+     [tracker.customVars addWithId:6 value:@"1" type:ATCustomVarTypeApp];
+     [tracker.customVars addWithId:7 value:@"" type:ATCustomVarTypeApp];
+     [tracker.customVars addWithId:8 value:@"" type:ATCustomVarTypeApp];
+     [tracker.customVars addWithId:9 value:@"20150101" type:ATCustomVarTypeApp];
+     [tracker.customVars addWithId:10 value:page type:ATCustomVarTypeApp];
+    
+     ATScreen *screen = [tracker.screens addWithName: @( msg ) ];
     screen.level2 = 34;
 
-    [tracker.customVars addWithId:1 value:@"68" type:ATCustomVarTypeScreen]; 
-    [tracker.customVars addWithId:2 value:@"34" type:ATCustomVarTypeScreen];
-    [tracker.customVars addWithId:3 value:@"" type:ATCustomVarTypeScreen];
-    [tracker.customVars addWithId:4 value:@"" type:ATCustomVarTypeScreen];
-    [tracker.customVars addWithId:5 value:chapter type:ATCustomVarTypeScreen]; 
-    [tracker.customVars addWithId:6 value:@"1" type:ATCustomVarTypeScreen];
-    [tracker.customVars addWithId:7 value:@"" type:ATCustomVarTypeScreen];
-    [tracker.customVars addWithId:8 value:@"" type:ATCustomVarTypeScreen];
-    [tracker.customVars addWithId:9 value:@"20150101" type:ATCustomVarTypeScreen];
-    [tracker.customVars addWithId:10 value:page type:ATCustomVarTypeScreen];
-
-    [tracker.customVars addWithId:1 value:@"68" type:ATCustomVarTypeApp];
-    [tracker.customVars addWithId:2 value:@"34" type:ATCustomVarTypeApp];
-    [tracker.customVars addWithId:3 value:@"" type:ATCustomVarTypeApp];
-    [tracker.customVars addWithId:4 value:@"" type:ATCustomVarTypeApp];
-    [tracker.customVars addWithId:5 value:chapter type:ATCustomVarTypeApp];
-    [tracker.customVars addWithId:6 value:@"1" type:ATCustomVarTypeApp];
-    [tracker.customVars addWithId:7 value:@"" type:ATCustomVarTypeApp];
-    [tracker.customVars addWithId:8 value:@"" type:ATCustomVarTypeApp];
-    [tracker.customVars addWithId:9 value:@"20150101" type:ATCustomVarTypeApp];
-    [tracker.customVars addWithId:10 value:page type:ATCustomVarTypeApp]; 
-    
     if (tracker == nil) {
 	    NSLog(@"%@", @"default Instance is null before dispatch" );
 	    return;
